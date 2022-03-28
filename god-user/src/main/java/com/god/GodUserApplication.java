1f5e26a254cde7a2207d858067486724ac1ae5a8
ext.qusongyun1 <ext.qusongyun1@jd.com>
2022-03-28 15:13:27 +0800

    修改提交文件

---
+++ god-user/src/main/java/com/god/GodUserApplication.java
@@ -0,0 +1,13 @@
+package com.god;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GodUserApplication.class, args);
+    }
+
+}
